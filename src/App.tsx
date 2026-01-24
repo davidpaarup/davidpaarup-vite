6e22b2a67943e348011cb9eefce462892e0fdf71
davidpaarup <hi@davidpaarup.com>
2026-01-24 15:47:06 +0100

    Quitado curriculum

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
-import Resume from './components/Resume'
 import Gallery from './components/Gallery'
 import Footer from './components/Footer'
 import { LanguageProvider } from './context/LanguageContext'
 
 function App() {
-  const [activeTab, setActiveTab] = useState('cv')
-  
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [activeTab])
-
   return (
     <LanguageProvider>
       <div className="min-h-screen bg-white text-black font-sans selection:bg-black selection:text-white">
-        <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
+        <Navbar />
         
         <main>
-          {activeTab === 'cv' ? <Resume /> : <Gallery />}
+          <Gallery />
         </main>
 
         <Footer />
