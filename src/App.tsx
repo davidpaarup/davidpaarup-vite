2c7d2a207c987027759470fe8f6d1e13578b23dc
davidpaarup <hi@davidpaarup.com>
2026-01-24 20:33:20 +0100

    Revert "Quitado curriculum"
    This reverts commit 6e22b2a67943e348011cb9eefce462892e0fdf71.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
+import Resume from './components/Resume'
 import Gallery from './components/Gallery'
 import Footer from './components/Footer'
 import { LanguageProvider } from './context/LanguageContext'
 
 function App() {
+  const [activeTab, setActiveTab] = useState('cv')
+  
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [activeTab])
+
   return (
     <LanguageProvider>
       <div className="min-h-screen bg-white text-black font-sans selection:bg-black selection:text-white">
-        <Navbar />
+        <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
         
         <main>
-          <Gallery />
+          {activeTab === 'cv' ? <Resume /> : <Gallery />}
         </main>
 
         <Footer />
