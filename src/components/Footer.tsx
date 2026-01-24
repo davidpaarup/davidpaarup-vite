5eba7202a0c6e4b91bd5f07dfbaf5c131a9b126f
davidpaarup <hi@davidpaarup.com>
2026-01-24 20:33:18 +0100

    Revert "Correo"
    This reverts commit 07ed76cfe2408d63ff9e8a61d58bc35f362f33af.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
-//import {useLanguage} from "../context/UseLanguage.tsx";
+import {useLanguage} from "../context/UseLanguage.tsx";
 
 const Footer: React.FC = () => {
-  //const { t } = useLanguage();
+  const { t } = useLanguage();
   
   return (
     <footer className="border-t border-gray-100 py-12 mt-20">
       <div className="max-w-5xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
-        {/*<div className="text-sm text-gray-400">
+        <div className="text-sm text-gray-400">
           {t('footer.copy').replace('{year}', new Date().getFullYear().toString())}
-        </div>*/}
+        </div>
         <div className="flex gap-6 text-sm font-medium">
           {/* <a href="https://www.linkedin.com/in/davidpaarup/" target={"_blank"} className="text-gray-400 hover:text-black transition-colors">LinkedIn</a> */}
           {/* <a href="https://github.com/davidpaarup" target={"_blank"} className="text-gray-400 hover:text-black transition-colors">GitHub</a> */}
           {/* <a href="https://www.instagram.com/davidpaarup/" target={"_blank"} className="text-gray-400 hover:text-black transition-colors">Instagram</a> */}
-          <a href="mailto:hola@davidpaarup.com" className="text-gray-400 hover:text-black transition-colors">
-            Envíame un correo
-          </a>
+          <a href="mailto:hola@davidpaarup.com" className="text-gray-400 hover:text-black transition-colors">Email</a>
         </div>
       </div>
     </footer>
